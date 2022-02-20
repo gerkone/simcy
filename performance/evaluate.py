691a06c571293823e076cc0f4213bcd355321136
gerkone <g.galletti@celonis.com>
2022-02-20 15:11:06 +0100

    profile

--- performance/evaluate.py
+++ performance/evaluate.py
@@ -3,12 +3,18 @@ import importlib.util
 import simpy
 import simcy
 import random
+import cProfile, pstats
+import datetime
+
 
 if __name__ == "__main__":
     random.seed(0)
     performance_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "examples/")
 
     tests = [f for f in os.listdir(performance_dir) if ".py" in f]
+
+    profiler = cProfile.Profile()
+    profiler.enable()
 
     for test in tests:
         spec = importlib.util.spec_from_file_location("{}.run".format(test.split(".")[0]), os.path.join(performance_dir, test))
@@ -26,3 +32,15 @@ if __name__ == "__main__":
             avg_total_parts /= 10
 
             print("{} - {} - avg run: {:.0f}us".format(test, str(des.__name__), avg_run_time / 1000))
+
+    profiler.disable()
+
+    if not os.path.exists("profiling"):
+        os.makedirs("profiling")
+
+    stats = pstats.Stats(profiler).sort_stats('ncalls')
+    filename = "profiling/prof_{}".format(datetime.datetime.timestamp(datetime.datetime.now()))
+    stats.dump_stats(filename + ".pstat")
+    # --root='core:183:step'
+    os.popen(
+        "gprof2dot -f pstats {0}.pstat -n 1.0 -e 0.5 --color-nodes-by-selftime | dot -Tpng -o {0}.png".format(filename))
