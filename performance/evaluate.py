f3ff2e8d1de2370775fb5f78530292557d94eecb
gerkone <g.galletti@celonis.com>
2022-02-20 15:25:40 +0100

    profile

--- performance/evaluate.py
+++ performance/evaluate.py
@@ -13,14 +13,14 @@ if __name__ == "__main__":
 
     tests = [f for f in os.listdir(performance_dir) if ".py" in f]
 
-    profiler = cProfile.Profile()
-    profiler.enable()
+    for des in [simpy, simcy]:
+        profiler = cProfile.Profile()
+        profiler.enable()
 
-    for test in tests:
-        spec = importlib.util.spec_from_file_location("{}.run".format(test.split(".")[0]), os.path.join(performance_dir, test))
-        test_module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(test_module)
-        for des in [simpy, simcy]:
+        for test in tests:
+            spec = importlib.util.spec_from_file_location("{}.run".format(test.split(".")[0]), os.path.join(performance_dir, test))
+            test_module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(test_module)
             avg_run_time = 0
             avg_total_parts = 0
 
@@ -31,16 +31,18 @@ if __name__ == "__main__":
             avg_run_time /= 10
             avg_total_parts /= 10
 
-            print("{} - {} - avg run: {:.0f}us".format(test, str(des.__name__), avg_run_time / 1000))
+            print("{} - {} - avg run: {:.0f}us".format(str(des.__name__), test, avg_run_time / 1000))
 
-    profiler.disable()
+        profiler.disable()
 
-    if not os.path.exists("profiling"):
-        os.makedirs("profiling")
+        if not os.path.exists("profiling"):
+            os.makedirs("profiling".format(str(des.__name__)))
 
-    stats = pstats.Stats(profiler).sort_stats('ncalls')
-    filename = "profiling/prof_{}".format(datetime.datetime.timestamp(datetime.datetime.now()))
-    stats.dump_stats(filename + ".pstat")
-    # --root='core:183:step'
-    os.popen(
-        "gprof2dot -f pstats {0}.pstat -n 1.0 -e 0.5 --color-nodes-by-selftime | dot -Tpng -o {0}.png".format(filename))
+        stats = pstats.Stats(profiler).sort_stats('ncalls')
+        filename = "profiling/{}_prof_{}".format(str(des.__name__), datetime.datetime.timestamp(datetime.datetime.now()))
+        stats.dump_stats(filename + ".pstat")
+        # --root='core:183:step'
+        os.popen(
+            "gprof2dot -f pstats {0}.pstat -n 1.0 -e 0.5 --color-nodes-by-selftime | dot -Tpng -o {0}.png".format(filename))
+
+        print("--")
